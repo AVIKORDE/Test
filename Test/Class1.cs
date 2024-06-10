f23c219f4788f2c4b97c018eac7ef706d6884d34
Avinash Santosh Korde <kordeavinash86@gmail.com>
2024-06-10 15:33:57 +0530

    Add project files.

---
+++ Test/Class1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    #region
+    //public class Assignment1
+    //{
+    //    int i, len, vowels, consonants;
+    //    string str = "avinashkorde";
+    //    public void vowels1()
+    //    {
+
+
+    //        for (i = 0; i < str.Length; i++)
+    //        {
+
+
+    //            if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+    //            {
+
+
+    //                vowels++;
+    //            }
+    //            else if ((str[i] >= 'a' && str[i] <= 'z') ||
+    //                     (str[i] >= 'A' && str[i] <= 'Z'))
+    //            {
+    //                consonants++;
+    //            }
+    //        }
+    //        Console.WriteLine("Vowels" + vowels);
+    //        Console.WriteLine("Consonants" + consonants);
+    //    }
+    //}
+    #endregion
+    #region
+    //Assignement2
+    //public class person
+    //{
+    //    public string Name { get; set; }
+    //    public int Age { get; set; }
+    //    public person(string name, int age)
+    //    {
+    //        Name = name;
+    //        Age = age;
+
+    //    }
+    //    public virtual void show_info()
+    //    {
+    //        Console.WriteLine(Name);
+    //        Console.WriteLine(Age);
+    //    }
+    //}
+    //public class Teacher : person
+    //{
+    //    public int Salary { get; set; }
+    //    public Teacher(string name, int age, int salary) : base(name, age)
+    //    {
+    //        Salary = salary;
+    //    }
+    //    public override void show_info()
+    //    {
+    //        base.show_info();
+    //        Console.WriteLine(Salary); ;
+    //    }
+
+    //}
+    //public class Student : person
+    //{
+    //    public int total_Marks { get; set; }
+    //    public Student(string name, int age, int totalMarks)
+    //        : base(name, age)
+    //    {
+    //        total_Marks = totalMarks;
+    //    }
+    //    public override void show_info()
+    //    {
+    //        base.show_info();
+    //        Console.WriteLine(total_Marks);
+    //    }
+    #endregion
+    #region
+    //Assignment3
+    //public delegate int Mydelegate(int a, int b);
+    //class Delegate1
+    //{ 
+
+
+
+    //    public static int area(int length, int width)
+    //    {
+    //        return length * width;
+    //    }
+    //    public static int greater(int n1, int n2)
+    //    {
+    //        return n1 > n2 ? n1 : n2;
+    //    }
+    //    public static int getPower(int n, int p)
+    //    {
+    //        return (int)Math.Pow(n, p);
+    //    }
+    //}
+    #endregion
+    //Assignment4
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Contact(string name, string phoneNumber)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+        }
+    }
+    public class PhoneBook
+    {
+
+        public Contact this[string name]
+        {
+            get
+            {
+                return contact;
+            }
+            set
+            {
+                contacts[name] = value;
+            }
+        }
+
+
+        //internal class Class1
+        //{
+        //}
+    }
+    internal class Class1
+    {
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        #region
+        //Assignment1 ass= new Assignment1();
+        //ass.vowels1();
+        #endregion
+        #region
+        //    Student s1 = new Student("avi", 24, 70);
+        //    Student s2 = new Student("Rohan", 23, 80);
+        //    Student s3 = new Student("Mayuresh", 20, 90);
+
+        //    s1.show_info();
+        //    s2.show_info();
+        //        s3.show_info();
+        //    Teacher t1 = new Teacher("Pravin sir", 35, 50000);
+        //    Teacher t2 = new Teacher("Ashok Sir", 40, 60000);
+        //    Teacher t3 = new Teacher("Giri Sir", 40, 800000);
+        //    t1.show_info();
+        //    t2.show_info();
+        //    t3.show_info();
+        #endregion
+        #region
+        //Mydelegate Areadel = new Mydelegate(area);
+        //Mydelegate Greaterdel = new Mydelegate(greater);
+        //Mydelegate Getpowerd = new Mydelegate(getPower);
+        //int length = 5;
+        //int width = 10;
+        //int area = Areadel(length, width);
+        //Console.WriteLine(area);
+        //int n1 = 15;
+        //int n2 = 20;
+        //int greatest = Greaterdel(n1, n2);
+        //Console.WriteLine(greatest);
+        //int n = 2;
+        //int p = 3;
+        //int power = Getpowerd(n, p);
+        //Console.WriteLine(power);
+        #endregion;
+    }
+}
